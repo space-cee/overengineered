1eb14218349ca3ab32567fbc9a5dd9f751f9be24
secretnoe-rblx <chocofly0609@gmail.com>
2025-11-05 10:15:54 +0500

    relative-vector-block | edit description

--- src/shared/blocks/blocks/RelativeVectorBlock.ts
+++ src/shared/blocks/blocks/RelativeVectorBlock.ts
@@ -40,7 +40,7 @@ export const RelativeVectorBlock = {
 	...BlockCreation.defaults,
 	id: "RelativeVectorblock",
 	displayName: "Relative Vector",
-	description: "Sets the vector's landmarks in the right-handed system",
+	description: "Returns the relative rotation vectors in space",
 	logic: { definition, ctor: Logic },
 	modelSource: {
 		model: BlockCreation.Model.fAutoCreated("TripleGenericLogicBlockPrefab", "REL-VEC"),
